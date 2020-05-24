59137b79be613cf59ea516d02506cb3b50a3403c
rezkiy37 <rezkiy.mx@gmail.com>
2020-05-25 02:50:50 +0300

    intro development

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log('JS ready!');
+console.log('JS ready!')
+
+const body = document.querySelector('body')
+const header = document.querySelector('#header')
+const main = document.querySelector('#intro')
+
+let headerHeight = header.clientHeight
+
+//body padding
+body.style.paddingTop = `${headerHeight}px`
+//intro height
+main.style.height = `${window.innerHeight - headerHeight}px`
