4f0a9c47fed895dabb49a4157ff9fe5e19a63d96
rezkiy37 <rezkiy.mx@gmail.com>
2020-06-04 20:40:08 +0300

    Quote had created

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,32 @@ addEventListener('resize', () => {
     //intro height
     main.style.height = `${window.innerHeight - headerHeight}px`
 })
+
+
+//quote anim
+const quoteDesc = document.querySelector('#quoteDesc')
+const quoteDescList = document.querySelector('#quoteDescList')
+const quotePicture = document.querySelector('#quotePicture')
+const quoteImg = document.querySelector('#quoteImg')
+const quoteheader = document.querySelector('#quoteheader')
+
+let quoteHeight = quoteheader.offsetHeight
+quoteDesc.style.height = `${quoteHeight + 20}px`
+
+addEventListener('resize', () => {
+    let quoteHeight = quoteheader.offsetHeight
+    quoteDesc.style.height = `${quoteHeight + 20}px`
+})
+
+quoteDesc.addEventListener('mouseover', e => {
+    quotePicture.classList.add('active')
+    quoteDesc.style.overflowY = 'auto'
+    quoteDesc.style.height = '95%'
+})
+
+quoteDesc.addEventListener('mouseleave', e => {
+    quoteDesc.scrollTo(0, 0)
+    quoteDesc.style.overflowY = 'hidden'
+    quotePicture.classList.remove('active')
+    quoteDesc.style.height = `${quoteHeight + 30}px`
+})
